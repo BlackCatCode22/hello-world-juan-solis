7ec626d0f0b5bf6257883138e7478bb1046a854d
juan-solis <76581125+juan-solis@users.noreply.github.com>
2024-01-23 22:44:16 -0800

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,15 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+    System.out.println("Hello world!");
+
+    int num1 = 2;
+    int num2 = 4;
+    int sum;
+
+    sum = num1 + num2;
+
+    System.out.println("\n\n The sum of " + num1 + " and " + num2 + " is: " + sum + "\n\n");
+    }
+}
